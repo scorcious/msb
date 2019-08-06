177b4ce5bf8b3f35e234dca49ede8e9268c7f0fd
Richard Sugianto So <scyrs1@nottingham.edu.cn>
2019-08-06 17:00:27 +0800

    pages controller -- home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   def home
     @no_search_bar = true
+    @no_container = true
   end
 
   def index
