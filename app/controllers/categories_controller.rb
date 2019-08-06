d8093406d3bea678c520774d94696bc318acd131
steven ly <shiwenly@gmail.com>
2019-08-06 15:14:16 +0800

    profile and categorie

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,37 @@
+class CategoriesController < ApplicationController
+
+  def new
+    @category = Category.new
+    authorize @category
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.user = current_user
+    authorize @category
+    if @category.save!
+      redirect_to profile_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @category = Category.find(paramas[:id])
+    authorize @category
+  end
+
+  def update
+    @category = Category.find(paramas[:id])
+    authorize @category
+    @category.update(category_params)
+    redirect_to profile_path(current_user)
+  end
+
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :level)
+  end
+end
