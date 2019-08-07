c1eab2c18ece19c3b4201062c6bc336b8a2c856d
Felipe Roschel <f.roschel@gmail.com>
2019-08-07 13:50:42 +0800

    friends table relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_093155) do
+ActiveRecord::Schema.define(version: 2019_08_07_053652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,17 @@ ActiveRecord::Schema.define(version: 2019_08_06_093155) do
     t.datetime "updated_at", null: false
     t.index ["match_id"], name: "index_forums_on_match_id"
     t.index ["user_id"], name: "index_forums_on_user_id"
+  end
+
+  create_table "friendships", id: :serial, force: :cascade do |t|
+    t.string "friendable_type"
+    t.integer "friendable_id"
+    t.integer "friend_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "blocker_id"
+    t.integer "status"
+    t.index ["friendable_id", "friend_id"], name: "index_friendships_on_friendable_id_and_friend_id", unique: true
   end
 
   create_table "matches", force: :cascade do |t|
