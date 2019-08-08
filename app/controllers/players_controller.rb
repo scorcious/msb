f71b777395e2bca20e3f2213149f7687f3474176
Felipe Roschel <f.roschel@gmail.com>
2019-08-08 13:25:16 +0800

    challenge your buddies on match#show

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -16,16 +16,30 @@ class PlayersController < ApplicationController
       authorize @player = Player.new
       @match = Match.find(params[:match_id])
 
-      challenger = @match.players.select { |p| p.user == current_user }
+      challenger = @match.players.where(user_id: current_user).first
+
+      challenger = match_auto_join if challenger.nil?
 
       @player.match = @match
       @player.user = User.find(params[:player][:user_id])
-      @player.team = challenger[0].team == 'A' ? 'B' : 'A'
+
+      @player.team = challenger.team == 'A' ? 'B' : 'A'
       @player.status = 'pending'
 
       if @player.save!
         redirect_to match_path(@match)
       end
     end
+  end
+
+  private
+
+  def match_auto_join
+    Player.create!(
+      match_id: @match.id,
+      user_id: current_user.id,
+      team: 'A',
+      status: 'accepted'
+    )
   end
 end
