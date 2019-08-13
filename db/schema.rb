50b41406db98e6f6e4afc7426d2c694ee5e47875
steven ly <shiwenly@gmail.com>
2019-08-13 12:43:51 +0800

    changed database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_07_053652) do
+ActiveRecord::Schema.define(version: 2019_08_13_042816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_08_07_053652) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "level"
-    t.integer "points", default: 0
+    t.integer "points", default: 300
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
