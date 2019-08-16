f7b794be8921a98b31010d753c238f463aecf1cd
Richard Sugianto So <scyrs1@nottingham.edu.cn>
2019-08-16 14:38:55 +0800

    contact page skip user authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :index, :contacts]
   def home
     @no_search_bar = true
     @no_container = true
